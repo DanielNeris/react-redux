38a7b20c4bf8910cead6f194c21c480f148d3f10
Daniel de Souza Neri <danielneris01@gmail.com>
2019-07-04 19:43:27 -0300

    i dont know

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-function toggleLesson (module, lesson){
-    return {
-        type: 'TOGGLE_LESSON',
-        module,
-        lesson,
-    };
-}
+import * as CourseActions from '../../store/actions/course';
 
-
-const Sidebar = ({ modules, dispatch }) => (
+const Sidebar = ({ modules, toggleLesson }) => (
     <aside>
         {modules.map(module => (
             <div key={module.id}>
@@ -20,7 +14,7 @@ const Sidebar = ({ modules, dispatch }) => (
                     {module.lessons.map(lesson => (
                         <li key={lesson.id}>
                             {lesson.title}
-                            <button onClick={() => dispatch(toggleLesson(module, lesson))}>Selecionar</button>
+                            <button onClick={() => toggleLesson(module, lesson)}>Selecionar</button>
                         </li>
                     ))}
                 </ul>
@@ -29,4 +23,10 @@ const Sidebar = ({ modules, dispatch }) => (
     </aside>
 );
 
-export default connect(state => ({ modules: state.modules }))(Sidebar);
+const mapStateToProps = state => ({
+    modules: state.course.modules
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(CourseActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
